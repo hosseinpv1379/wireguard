221bde6e1a81cf2feb351379f25c9b14be03703d
hosseinpv1379 <hosseinzarepv@gmail.com>
2025-02-12 08:57:06 -0800

    1

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ BOT_TOKEN = "7634279369:AAEtvM009A-LT4B73yy1LAtuCKalS1mDsDo"
 DB_HOST = "localhost"
 DB_USER = "wireguard_user"
 DB_PASSWORD = "Hossein-1379"
-DB_NAME = "WireGuad"
+DB_NAME = "WireGuard"
 
 # Other configurations
 ADMIN_USER_IDS = [  # تلگرام آیدی ادمین‌ها
